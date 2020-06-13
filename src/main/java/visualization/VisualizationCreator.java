ec3a01d7fdf563c829e882288e43d548fb9a69ec
kamsza <kamsza@onet.pl>
2020-06-13 21:33:58 +0200

    create module for visualization

---
+++ src/main/java/visualization/VisualizationCreator.java
@@ -0,0 +1,17 @@
+package visualization;
+
+import processing.core.PApplet;
+
+public class VisualizationCreator extends PApplet {
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
