604c1e39ba4c83632652fd8bbaef1d5640a81cf6
Michał <barczykmt@gmail.com>
2020-07-18 17:27:35 +0200

    change waymo data properties to Float since Float maps well during java code generation

---
+++ src/main/java/waymoadapter/model/waymo/lidar/Label.java
@@ -0,0 +1,31 @@
+
+package mapper.model.waymo.lidar;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "box",
+    "metadata",
+    "type",
+    "id",
+        "detectionDifficultyLevel",
+        "trackingDifficultyLevel"
+})
+public class Label {
+
+    @JsonProperty("box")
+    public Box box;
+    @JsonProperty("metadata")
+    public Metadata metadata;
+    @JsonProperty("type")
+    public Type type;
+    @JsonProperty("id")
+    public String id;
+    @JsonProperty("detectionDifficultyLevel")
+    public String detectionDifficultyLevel;
+    @JsonProperty("trackingDifficultyLevel")
+    public String trackingDifficultyLevel;
+}
---
+++ src/main/java/waymoadapter/model/waymo/lidar/VehicleSide.java
@@ -0,0 +1,13 @@
+package mapper.model.waymo.lidar;
+
+public enum VehicleSide {
+    ON_THE_LEFT,
+    ON_THE_RIGHT,
+    IN_FRONT_OF,
+
+    FRONT,
+    FRONT_LEFT,
+    FRONT_RIGHT,
+    SIDE_LEFT,
+    SIDE_RIGHT
+}
