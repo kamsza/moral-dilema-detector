4b23d16b2cb0851a2faaa5da34fec05c2c6853c1
kamsza <kamsza@onet.pl>
2020-10-13 13:29:03 +0200

    Car overtaking another car scenariio added

--- src/main/java/generator/ModelBuilder.java
+++ src/main/java/generator/ModelBuilder.java
@@ -1,5 +1,6 @@
 package generator;
 
+import javafx.util.Pair;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import project.*;
 
@@ -65,32 +66,40 @@ public class ModelBuilder {
     }
 
     // CAR - CAR SCENARIOS
+    private Pair<Float, Vehicle> getRandomVehicle() {
+        int r = ProbRand.randInt(new int[]{1, 2, 3}, new double[]{0.2, 0.1, 0.1});
+        Vehicle vehicle;
+        float entitySize;
+
+        switch (r) {
+            case 1:
+                vehicle = factory.createTruck(ObjectNamer.getName("vehicle"));
+                entitySize = sizeManager.getLength("truck");
+                break;
+            case 2:
+                vehicle = factory.createMotorcycle(ObjectNamer.getName("vehicle"));
+                entitySize = sizeManager.getLength("motorcycle");
+                break;
+            case 3:
+                vehicle = factory.createBicycle(ObjectNamer.getName("vehicle"));
+                entitySize = sizeManager.getLength("bike");
+                break;
+            default:
+                vehicle = factory.createCar(ObjectNamer.getName("vehicle"));
+                entitySize = sizeManager.getLength("car");
+                break;
+        }
+
+        return new Pair(entitySize, vehicle);
+    }
+
     public ModelBuilder addVehicles(int[] objectsNum, double[] prob) {
         int N = ProbRand.randInt(objectsNum, prob);
 
         for(int i = 0; i < N; i++) {
-            int r = ProbRand.randInt(new int[]{1, 2, 3}, new double[]{0.2, 0.1, 0.1});
-            Vehicle vehicle;
-            float entitySize;
-
-            switch (r) {
-                case 1:
-                    vehicle = factory.createTruck(ObjectNamer.getName("vehicle"));
-                    entitySize = sizeManager.getLength("truck");
-                    break;
-                case 2:
-                    vehicle = factory.createMotorcycle(ObjectNamer.getName("vehicle"));
-                    entitySize = sizeManager.getLength("motorcycle");
-                    break;
-                case 3:
-                    vehicle = factory.createBicycle(ObjectNamer.getName("vehicle"));
-                    entitySize = sizeManager.getLength("bike");
-                    break;
-                default:
-                    vehicle = factory.createCar(ObjectNamer.getName("vehicle"));
-                    entitySize = sizeManager.getLength("car");
-                    break;
-            }
+            Pair<Float, Vehicle>  randomVehicle = getRandomVehicle();
+            float entitySize = randomVehicle.getKey();
+            Vehicle vehicle = randomVehicle.getValue();
 
             int laneNo = randomPositioner.getRandomLaneNumber(entitySize);
             float distance = randomPositioner.getRandomDistance(laneNo, entitySize);
@@ -101,8 +110,22 @@ public class ModelBuilder {
     }
 
     public ModelBuilder addOvertakenVehicle() {
-        // TODO
-        return this;
+        if(model.getRoadType().getLanes_count().iterator().next() == 1 || model.getRoadType().getLanes_count().iterator().next() -1 == model.getRoadType().getMain_vehicle_lane_id().iterator().next())
+            return this;
+
+        int lanesMovingLeft = model.getRoadType().getLeft_lanes_count().iterator().next();
+        int lanesMovingRight = model.getRoadType().getRight_lanes_count().iterator().next();
+        model.getRoadType().removeLeft_lanes_count(lanesMovingLeft);
+        model.getRoadType().removeRight_lanes_count(lanesMovingRight);
+        model.getRoadType().addLeft_lanes_count(lanesMovingLeft + 1);
+        model.getRoadType().addRight_lanes_count(lanesMovingRight - 1);
+
+        Pair<Float, Vehicle>  randomVehicle = getRandomVehicle();
+        Vehicle vehicle = randomVehicle.getValue();
+
+        int laneNo = model.getRoadType().getMain_vehicle_lane_id().iterator().next() + 1;
+        float distance = rand.nextFloat() * 200 - 100F;
+        return addVehicle(vehicle, laneNo, distance);
     }
 
     public ModelBuilder addApproachedVehicle() {
