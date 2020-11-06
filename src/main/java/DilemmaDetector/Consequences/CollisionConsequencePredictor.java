ba34def173ab6c2819578aad968c8320348fefdd
Mateusz Nabywaniec <mateusz.nabywaniec@gmail.com>
2020-11-06 22:02:20 +0100

    Different consequences for passengers in main vehicle and consequences for pedestrians

--- src/main/java/DilemmaDetector/Consequences/CollisionConsequencePredictor.java
+++ src/main/java/DilemmaDetector/Consequences/CollisionConsequencePredictor.java
@@ -23,15 +23,25 @@ public class CollisionConsequencePredictor {
         this.model = model;
     }
 
+    private boolean isPedestrian(Actor victimActor, Living_entity victim){
+        return victimActor.getEntityName().equals(victim.getOwlIndividual().getIRI().toString());
+    }
+
     public void createCollisionConsequences(Decision decision, Actor victimActor, Actor other) {
         List<Living_entity> individualVictims = getLivingEntitiesFromActor(victimActor);
-
         double speed = getCollisionSpeed(victimActor.getRigidBody(), other.getRigidBody());
         double materialConsequenceValue = getMaterialConsequence(victimActor, speed);
         double materialConsequenceValueOther = getMaterialConsequence(other, speed);
         for (Living_entity victim : individualVictims) {
-            ConsequenceType consequenceType = getHealthConsequenceType(
-                    getCollisionSpeed(victimActor.getRigidBody(), other.getRigidBody()));
+            ConsequenceType consequenceType;
+            if (isPedestrian(victimActor, victim)) {
+                //System.out.println("ARE PEDESTRIANS : " + victimActor.getEntityName() + "   " + victim.getOwlIndividual().getIRI().toString());
+                consequenceType = getHealthConsequenceTypeForPedestrian(
+                        getCollisionSpeed(victimActor.getRigidBody(), other.getRigidBody()));
+            } else {
+                 consequenceType = getHealthConsequenceType(
+                        getCollisionSpeed(victimActor.getRigidBody(), other.getRigidBody()));
+            }
             if (consequenceType != ConsequenceType.NO_CONSEQUENCE) {
                 consequenceContainer.addHealthConsequence(decision, victim, consequenceType);
             }
@@ -71,6 +81,26 @@ public class CollisionConsequencePredictor {
         else{
             return (int) Math.round(victimValue * (speed / criticalSpeed));
         }
+    }
+
+    private ConsequenceType getHealthConsequenceTypeForPedestrian(double speedOfCollision) {
+        double killProbability = pedestrianFatalInjuryProbability(speedOfCollision);
+        double severInjuryProbability = pedestrianSeverInjuryProbability(speedOfCollision);
+        double lightInjuryProbability = pedestrianMinorInjuryProbability(speedOfCollision);
+        double maxProbability = killProbability;
+        if (maxProbability < severInjuryProbability)
+            maxProbability = severInjuryProbability;
+        if (maxProbability < lightInjuryProbability)
+            maxProbability = lightInjuryProbability;
+
+        if (maxProbability == killProbability)
+            return ConsequenceType.KILLED;
+        if (maxProbability == severInjuryProbability)
+            return ConsequenceType.SEVERELY_INJURED;
+        if (maxProbability == lightInjuryProbability)
+            return ConsequenceType.LIGHTLY_INJURED;
+
+        return ConsequenceType.NO_CONSEQUENCE;
     }
 
     private ConsequenceType getHealthConsequenceType(double speedOfCollision) {
@@ -165,6 +195,53 @@ public class CollisionConsequencePredictor {
         }
         return probability;
     }
+
+    private double pedestrianFatalInjuryProbability(double speed) {
+        speed = PhysicsUtils.MetersToKmph(speed);
+        double probability;
+        if (speed < 35) {
+            probability = getProbabilityFromLinearFunction(0, 0, 35, 0.1, speed);
+        } else if (speed < 50) {
+            probability = getProbabilityFromLinearFunction(35, 0.1, 50, 0.4, speed);
+        } else if (speed < 65) {
+            probability = getProbabilityFromLinearFunction(50, 0.4, 65, 0.9, speed);
+        } else {
+            probability = getProbabilityFromLinearFunction(65, 0.9, 80, 1, speed);
+        }
+        return probability;
+    }
+
+
+    private double pedestrianMinorInjuryProbability(double speed) {
+        speed = PhysicsUtils.MetersToKmph(speed);
+        double probability;
+        if (speed < 35) {
+            probability = getProbabilityFromLinearFunction(0, 0, 35, 0.9, speed);
+        } else if (speed < 50) {
+            probability = getProbabilityFromLinearFunction(35, 0.9, 50, 0.6, speed);
+        } else if (speed < 65) {
+            probability = getProbabilityFromLinearFunction(50, 0.6, 65, 0.1, speed);
+        } else {
+            probability = getProbabilityFromLinearFunction(65, 0.6, 80, 0, speed);
+        }
+        return probability;
+    }
+
+    private double pedestrianSeverInjuryProbability(double speed) {
+        speed = PhysicsUtils.MetersToKmph(speed);
+        double probability;
+        if (speed < 35) {
+            probability = getProbabilityFromLinearFunction(0, 0, 35, 0.2, speed);
+        } else if (speed < 50) {
+            probability = getProbabilityFromLinearFunction(35, 0.2, 50, 0.7, speed);
+        } else if (speed < 65) {
+            probability = getProbabilityFromLinearFunction(50, 0.7, 65, 0.4, speed);
+        } else {
+            probability = getProbabilityFromLinearFunction(65, 0.4, 80, 0.1, speed);
+        }
+        return probability;
+    }
+
 
     private double getProbabilityFromLinearFunction(double x1, double y1, double x2, double y2, double speed) {
         return (y2 - y1) / (x2 - x1) * (speed - x1) + y1;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -72,8 +72,8 @@ public class Main {
                 .build();
 
         for(int i=0; i<1; i++) {
-//            Model scenarioModel = getModelFromGenerator(factory);
-            Model scenarioModel = getModelFromReader(factory,197);
+            Model scenarioModel = getModelFromGenerator(factory);
+//            Model scenarioModel = getModelFromReader(factory,197);
 
             Set leftLanes = scenarioModel.getLanes().get(Model.Side.LEFT).entrySet();
             Set rightLanes =  scenarioModel.getLanes().get(Model.Side.RIGHT).entrySet();
@@ -105,14 +105,14 @@ public class Main {
                 for (Actor a : entry.getValue()) System.out.println(a.getEntity());
             }
 
-//            consequenceContainer.saveConsequencesToOntology();
+            consequenceContainer.saveConsequencesToOntology();
             System.out.println(mdd.detectMoralDilemma(scenarioModel));
 
-//            try {
-//                factory.saveOwlOntology();
-//            } catch (OWLOntologyStorageException ignored) {
-//            }
-//            Visualization.getImage(scenarioModel);
+            try {
+                factory.saveOwlOntology();
+            } catch (OWLOntologyStorageException ignored) {
+            }
+            Visualization.getImage(scenarioModel);
         }
     }
 
