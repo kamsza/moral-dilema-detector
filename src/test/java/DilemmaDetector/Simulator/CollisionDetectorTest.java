30f825b84118bfb2024520872170a74b4fbe2611
Mateusz Nabywaniec <mateusz.nabywaniec@gmail.com>
2020-11-15 15:03:08 +0100

    Update collision detector test

--- src/test/java/DilemmaDetector/Simulator/CollisionDetectorTest.java
+++ src/test/java/DilemmaDetector/Simulator/CollisionDetectorTest.java
@@ -1,44 +1,109 @@
 package DilemmaDetector.Simulator;
-
-import generator.BaseScenarioGenerator2;
 import generator.Model;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
-import project.MyFactory;
-import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Collections;
-import java.util.List;
+import org.semanticweb.owlapi.model.*;
+import project.Entity;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-public class CollisionDetectorTest {
-    public static final String baseIRI = "http://webprotege.stanford.edu/";
-    private Model model;
+public class CollisionDetectorTest{
+    private CollisionDetector collisionDetector;
+    private Actor vehicleActor;
 
     @Before
-    public void init() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, OWLOntologyCreationException {
-        OWLOntologyManager ontologyManager = OWLManager.createOWLOntologyManager();
-        OWLOntology ontology = ontologyManager.loadOntologyFromOntologyDocument(new File("src/main/resources/traffic_ontology.owl"));
-        MyFactory factory = new MyFactory(ontology);
-        BaseScenarioGenerator2 baseScenarioGenerator2 = new BaseScenarioGenerator2(factory, baseIRI);
-        this.model = baseScenarioGenerator2.generate();
+    public void init() {
+        RigidBody rigidBody = new RigidBody();
+        rigidBody.setWidth(2.0);
+        rigidBody.setLength(5.0);
+        rigidBody.setPosition(new Vector2(0, 0));
+
+        Entity entityMock = mock(Entity.class);
+        OWLNamedIndividual owlIndividualMock = mock(OWLNamedIndividual.class);
+        IRI iriMock = mock(IRI.class);
+        when(entityMock.getOwlIndividual()).thenReturn(owlIndividualMock);
+        when(owlIndividualMock.getIRI()).thenReturn(iriMock);
+        when(iriMock.toString()).thenReturn("entity");
+        vehicleActor = new Actor(entityMock, rigidBody);
+        collisionDetector = new CollisionDetector(mock(Model.class), vehicleActor, null, null);
     }
 
     @Test
-    public void detectCollisionInMomentTest(){
-        RigidBody rigidBody = new RigidBody();
-        rigidBody.setWidth(1.0);
-        rigidBody.setLength(1.0);
-        rigidBody.setPosition(new Vector2(2, 0));
+    public void shouldBeCollisionWithAllRigidBodies(){
+        //rigidBody in front of mainVehicle
+        RigidBody rigidBody1 = new RigidBody();
+        rigidBody1.setWidth(2.0);
+        rigidBody1.setLength(5.0);
+        rigidBody1.setPosition(new Vector2(3, 0));
 
-        Actor mainVehicle = new Actor(model.getVehicle(), RigidBodyMapper.rigidBodyForMainVehicle(model.getVehicle()));
-        List<Actor> actors = RigidBodyMapper.createActors(model);
-        List<Actor> surroundingActors = RigidBodyMapper.createSurroundingActors(model);
-        CollisionDetector collisionDetector = new CollisionDetector(model, mainVehicle, actors, surroundingActors);
-        Assert.assertTrue(collisionDetector.detectCollisionWithRigidBodyInMoment(rigidBody, "rigidBody"));
+        //rigidBody in the back of main Vehicle
+        RigidBody rigidBody2 = new RigidBody();
+        rigidBody2.setWidth(3.0);
+        rigidBody2.setLength(5.0);
+        rigidBody2.setPosition(new Vector2(-3, 2));
+
+        //rigidBody on right side of mainVehicle
+        RigidBody rigidBody3 = new RigidBody();
+        rigidBody3.setWidth(3.0);
+        rigidBody3.setLength(1.0);
+        rigidBody3.setPosition(new Vector2(0, -2));
+
+        //rigidBody on front left side of mainVehicle
+        RigidBody rigidBody4 = new RigidBody();
+        rigidBody4.setWidth(3.0);
+        rigidBody4.setLength(5.0);
+        rigidBody4.setPosition(new Vector2(2, 2));
+
+        //rigidBody on the back and left side
+        RigidBody rigidBody5 = new RigidBody();
+        rigidBody5.setWidth(3.0);
+        rigidBody5.setLength(5.0);
+        rigidBody5.setPosition(new Vector2(-3, 2));
+
+        Assert.assertTrue(collisionDetector.detectCollisionWithRigidBodyInMoment(rigidBody1, "rigidBody1"));
+        Assert.assertTrue(collisionDetector.detectCollisionWithRigidBodyInMoment(rigidBody2, "rigidBody2"));
+        Assert.assertTrue(collisionDetector.detectCollisionWithRigidBodyInMoment(rigidBody3, "rigidBody3"));
+        Assert.assertTrue(collisionDetector.detectCollisionWithRigidBodyInMoment(rigidBody4, "rigidBody4"));
+        Assert.assertTrue(collisionDetector.detectCollisionWithRigidBodyInMoment(rigidBody5, "rigidBody5"));
+    }
+
+    @Test
+    public void shouldNotBeCollisionWithAnyRigidBodies(){
+        //rigidBody in front of mainVehicle
+        RigidBody rigidBody1 = new RigidBody();
+        rigidBody1.setWidth(2.0);
+        rigidBody1.setLength(5.0);
+        rigidBody1.setPosition(new Vector2(6, 0));
+
+        //rigidBody in the back of main Vehicle
+        RigidBody rigidBody2 = new RigidBody();
+        rigidBody2.setWidth(3.0);
+        rigidBody2.setLength(5.0);
+        rigidBody2.setPosition(new Vector2(-6, 2));
+
+        //rigidBody on right side of mainVehicle
+        RigidBody rigidBody3 = new RigidBody();
+        rigidBody3.setWidth(3.0);
+        rigidBody3.setLength(1.0);
+        rigidBody3.setPosition(new Vector2(0, -4));
+
+        //rigidBody on front left side of mainVehicle
+        RigidBody rigidBody4 = new RigidBody();
+        rigidBody4.setWidth(3.0);
+        rigidBody4.setLength(5.0);
+        rigidBody4.setPosition(new Vector2(6, 4));
+
+        //rigidBody on the back and left side
+        RigidBody rigidBody5 = new RigidBody();
+        rigidBody5.setWidth(3.0);
+        rigidBody5.setLength(5.0);
+        rigidBody5.setPosition(new Vector2(-6, 4));
+
+        Assert.assertFalse(collisionDetector.detectCollisionWithRigidBodyInMoment(rigidBody1, "rigidBody1"));
+        Assert.assertFalse(collisionDetector.detectCollisionWithRigidBodyInMoment(rigidBody2, "rigidBody2"));
+        Assert.assertFalse(collisionDetector.detectCollisionWithRigidBodyInMoment(rigidBody3, "rigidBody3"));
+        Assert.assertFalse(collisionDetector.detectCollisionWithRigidBodyInMoment(rigidBody4, "rigidBody4"));
+        Assert.assertFalse(collisionDetector.detectCollisionWithRigidBodyInMoment(rigidBody5, "rigidBody5"));
     }
 }
