d36d6e84b994a4c5f8644977cfaae4b181b47430
MaciejBanaś <maciejb.98@gmail.com>
2020-12-08 12:06:02 +0100

    GUI works also with lane tile

--- src/main/java/commonadapter/adapters/nds/gui/NdsClientDashboard.java
+++ src/main/java/commonadapter/adapters/nds/gui/NdsClientDashboard.java
@@ -10,17 +10,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class NdsClientDashboard extends JFrame implements ActionListener {
-    private String ndsRoadJsonFilePath;
+    private String roadTileFilePath;
+    private String laneTileFilePath;
 
-    private final int baseHeight = 200;
-    private final int baseWidth = 460;
     private final int marginSize = 20;
     private final int blockHeight = 50;
     private final int blockWidth = 400;
+    private final int baseWidth = 460;
+    private final int baseHeight = 6 * marginSize + 3 * blockHeight;
 
-    private JButton selectNdsDataButton;
+    private JButton selectRoutingTileButton;
+    private JButton selectLaneTileButton;
     private JButton generateButton;
     private JTextArea createdRoadTextField;
+    private JTextArea createdLaneTextField;
 
     public NdsClientDashboard() {
         setSize(baseWidth, baseHeight);
@@ -31,57 +34,81 @@ public class NdsClientDashboard extends JFrame implements ActionListener {
         prepareDashboard();
     }
 
-    private void prepareDashboard() {
-        selectNdsDataButton = new JButton("Select file with ontology");
-
-        selectNdsDataButton.setBounds(marginSize, marginSize, blockWidth, blockHeight);
-        selectNdsDataButton.addActionListener(this);
-        add(selectNdsDataButton);
-
-        generateButton = new JButton("Generate");
-        generateButton.setBounds(marginSize, 2 * marginSize + blockHeight, blockWidth, blockHeight);
-        generateButton.addActionListener(this);
-        add(generateButton);
+    private JButton createButton(String description, Position pos) {
+        JButton button = new JButton(description);
+        button.setBounds(pos.getX(), pos.getY(), pos.getWidth(), pos.getHeight());
+        button.addActionListener(this);
+        add(button);
+        return button;
+    }
+    private JTextArea createArea(Position pos) {
+        JTextArea field = new JTextArea();
+        field.setBounds(pos.getX(), pos.getY(), pos.getWidth(), pos.getHeight());
+        add(field);
+        return field;
     }
 
-    private void prepareNewDashboard(int numberOfIds) {
-        int singleLineSize = 20;
-        setSize(baseWidth, baseHeight + singleLineSize * numberOfIds + marginSize);
+    private void prepareDashboard() {
+        selectRoutingTileButton = createButton("Select file with NDS RoutingTile",
+                new Position(marginSize, marginSize, blockWidth, blockHeight));
+        selectLaneTileButton = createButton("Select file with NDS LaneTile",
+                new Position(marginSize, 2 * marginSize + blockHeight, blockWidth, blockHeight));
+        generateButton = createButton("Generate",
+                new Position(marginSize, 3 * marginSize + 2 * blockHeight, blockWidth, blockHeight));
+    }
 
-        createdRoadTextField = new JTextArea();
-        createdRoadTextField.setBounds(marginSize, 3 * marginSize + 2 * blockHeight, blockWidth, singleLineSize * numberOfIds);
-        add(createdRoadTextField);
+    private void prepareNewDashboard(int numberOfRoadIds, int numberOfLaneIds) {
+        int singleLineSize = 20;
+        setSize(baseWidth, baseHeight + singleLineSize * (numberOfRoadIds + numberOfLaneIds) + marginSize * 2);
+        createdRoadTextField = createArea(new Position(marginSize, 4 * marginSize + 3 * blockHeight,
+                blockWidth, singleLineSize * numberOfRoadIds));
+        createdLaneTextField = createArea(new Position(marginSize,5 * marginSize + 3 * blockHeight + singleLineSize * numberOfRoadIds,
+                blockWidth, singleLineSize * numberOfLaneIds));
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         Object eventSource = e.getSource();
-        if (eventSource == selectNdsDataButton) selectNdsDataAction();
+        if (eventSource == selectRoutingTileButton) selectRoutingTileAction();
+        if (eventSource == selectLaneTileButton) selectLaneTileAction();
         if (eventSource == generateButton) generateAction();
     }
 
-
-    private void selectNdsDataAction() {
+    private String selectNdsFileAction(String description) {
         JFileChooser chooser = new JFileChooser();
-        FileNameExtensionFilter filter = new FileNameExtensionFilter(
-                "NDS routing tile", "json");
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(description, "json");
         chooser.setFileFilter(filter);
 
-        int returnVal = chooser.showOpenDialog(this);
-
-        if (returnVal == JFileChooser.APPROVE_OPTION) {
-            ndsRoadJsonFilePath = chooser.getSelectedFile().getAbsolutePath();
+        int path = chooser.showOpenDialog(this);
+        if (path == JFileChooser.APPROVE_OPTION) {
+            return chooser.getSelectedFile().getAbsolutePath();
         }
+        return null;
+    }
+
+    private void selectRoutingTileAction() {
+        this.roadTileFilePath = selectNdsFileAction("NDS routing tile");
+    }
+
+    private void selectLaneTileAction() {
+        this.laneTileFilePath = selectNdsFileAction("NDS lane tile");
     }
 
     private void generateAction() {
-        RoadBuilder builder = new RoadBuilder(ndsRoadJsonFilePath);
-        List<String> createdRoadsId = builder.buildRoads();
-        prepareNewDashboard(createdRoadsId.size());
-        String createdRoadsIdString = createdRoadsId.stream()
+        RoadBuilder builder = new RoadBuilder(roadTileFilePath, laneTileFilePath);
+        List<String> createdRoadIds = builder.buildRoads();
+        List<String> createdLaneIds = builder.buildLanes();
+        prepareNewDashboard(createdRoadIds.size(), createdLaneIds.size());
+
+        String createdRoadsIdString = createdRoadIds.stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining("\n"));
         createdRoadTextField.setText(createdRoadsIdString);
+
+        String createdLaneIdString = createdLaneIds.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining("\n"));
+        createdLaneTextField.setText(createdLaneIdString);
     }
 
 }
---
+++ src/main/java/commonadapter/adapters/nds/gui/Position.java
@@ -0,0 +1,31 @@
+package commonadapter.adapters.nds.gui;
+
+public class Position {
+    private final int x;
+    private final int y;
+    private final int width;
+    private final int height;
+
+    public Position(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
